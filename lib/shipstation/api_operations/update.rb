282bafd7b0f64647098c2cc5af8cd177ad5d906f
Tom Dallimore <me@tomdallimore.com>
2018-03-06 18:17:56 +0700

    Fixed bug with params in update request.

--- lib/shipstation/api_operations/update.rb
+++ lib/shipstation/api_operations/update.rb
@@ -3,7 +3,7 @@ module Shipstation
    	module Update
 			
 			def update object_id, params={}
-				Shipstation.request(:put, "#{class_name.downcase.pluralize}/#{id}", params)
+				Shipstation.request(:put, "#{class_name.downcase.pluralize}/#{object_id}", params)
 			end
 			
 		end
