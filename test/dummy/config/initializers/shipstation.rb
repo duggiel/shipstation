44bafb36bbbe318932a020ed854beb688662e0cd
Alessandro Desantis <desa.alessandro@gmail.com>
2021-01-22 13:38:47 +0100

    Remove Test::Unit configuration

--- test/dummy/config/initializers/shipstation.rb
+++
@@ -1,2 +0,0 @@
-Shipstation.username = '9d5a864d104b49a293d3ff2601ac89b4'
-Shipstation.password = 'ba3adb5094aa45b8aac9b354c7c6912a'
--- test/shipstation_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ShipstationTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Shipstation
-  end
-end
