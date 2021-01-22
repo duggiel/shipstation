697d2d8be21932cd14ccb2c618a80457eb7f00dc
Alessandro Desantis <desa.alessandro@gmail.com>
2021-01-22 13:41:02 +0100

    Write basic spec for `Shipstation.request`

---
+++ spec/shipstation_spec.rb
@@ -0,0 +1,40 @@
+RSpec.describe Shipstation do
+  describe '.request' do
+    it 'accepts the input as a keyword argument' do
+      request = stub_request(:post, /ssapi\.shipstation\.com/).with(
+        body: '{"key":"value"}',
+        headers: {
+          'Authorization'=>'Basic dXNlcm5hbWU6cGFzc3dvcmQ='
+        },
+      )
+
+      Shipstation.request(
+        :post,
+        'endpoint',
+        username: 'username',
+        password: 'password',
+        input: { 'key' => 'value' },
+      )
+
+      expect(request).to have_been_requested
+    end
+
+    it 'accepts the input as the only argument' do
+      allow(Shipstation).to receive_messages(username: 'username', password: 'password')
+      request = stub_request(:post, /ssapi\.shipstation\.com/).with(
+        body: '{"key":"value"}',
+        headers: {
+          'Authorization'=>'Basic dXNlcm5hbWU6cGFzc3dvcmQ='
+        },
+      )
+
+      Shipstation.request(
+        :post,
+        'endpoint',
+        { 'key' => 'value' },
+      )
+
+      expect(request).to have_been_requested
+    end
+  end
+end
